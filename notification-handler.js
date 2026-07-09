0c8a61ff9c41a8f5f3b747fc70591b82c92ac553
Mattcpt18 <mcclenathan.matthew@gmail.com>
2026-07-08 20:51:52 -0500

    Add service worker registration and periodic sync

---
+++ notification-handler.js
@@ -0,0 +1,17 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js").then(async (reg) => {
+    if (Notification.permission === "default") {
+      await Notification.requestPermission();
+    }
+
+    if ("periodicSync" in reg) {
+      try {
+        await reg.periodicSync.register("check-protocol", {
+          minInterval: 24 * 60 * 60 * 1000
+        });
+      } catch (e) {
+        console.log("Periodic Sync not supported");
+      }
+    }
+  });
+}
